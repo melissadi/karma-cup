de1d42d92ded1c30d4dd5b58b5b7ff3d644e5ccb
melissadi <m.w.straus@gmail.com>
2019-08-01 14:36:12 -0400

    Added QR scanner to Admin dashboard

--- app/javascript/react/containers/AdminShowContainer.js
+++ app/javascript/react/containers/AdminShowContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { Link } from "react-router-dom"
 import SearchBar from '../components/SearchBar'
 import CustomerTile from '../components/CustomerTile'
+import QrReader from 'react-qr-reader'
 
 class AdminShowContainer extends Component {
   constructor(props) {
@@ -10,9 +11,33 @@ class AdminShowContainer extends Component {
       adminObject: {},
       searchResults: [],
       searchString: "",
+      showSearch: false,
+      showScanner: false
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleScan = this.handleScan.bind(this)
+    this.handleError = this.handleError.bind(this)
+    this.toggleSearch = this.toggleSearch.bind(this)
+    this.toggleScanner = this.toggleScanner.bind(this)
+  }
+
+  toggleSearch(event){
+    event.preventDefault()
+    if (this.state.showSearch){
+      this.setState({ showSearch: false })
+    } else {
+      this.setState({ showSearch: true })
+    }
+  }
+
+  toggleScanner(event){
+    event.preventDefault()
+    if (this.state.showScanner){
+      this.setState({ showScanner: false })
+    } else {
+      this.setState({ showScanner: true })
+    }
   }
 
   handleChange(event) {
@@ -36,9 +61,16 @@ class AdminShowContainer extends Component {
     .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
-  giveCustomerPoints(event){
-    event.preventDefault()
+  handleScan(data) {
+    if (data) {
+      this.setState({
+        qrResult: data
+      })
+    }
+  }
 
+  handleError(err) {
+    console.error(err)
   }
 
   componentDidMount(){
@@ -69,7 +101,7 @@ class AdminShowContainer extends Component {
 
     let users
     let i = 0
-    if (this.state.searchResults){
+    if (this.state.searchResults && this.state.showSearch){
       users = this.state.searchResults.map(user => {
         i ++
         return(
@@ -82,22 +114,51 @@ class AdminShowContainer extends Component {
       })
     }
 
-      return(
-      <div className="return">
-        <p>What would you like to do?</p>
-        <div className="rows option">
-          <Link to={`/admins/${this.props.match.params.id}/stores/${storeId}`}><h2>View {storeName}'s Rewards</h2></Link>
-        </div>
-        <div className="rows option">
-          <h2>Reward Customer</h2>
+    let search
+    if (this.state.showSearch){
+      search =
+        <div className="search-section">
           <SearchBar
             handleSubmit={this.handleSubmit}
             handleChange={this.handleChange}
             searchString={this.state.searchString}
           />
-          {users}
         </div>
+    }
+
+    let scanner
+    if (this.state.showScanner){
+      scanner =
+        <div className="scanner-section">
+          <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '100%' }}
+          />
+        </div>
+    }
+
+    return(
+    <div className="return">
+      <p>What would you like to do?</p>
+      <div className="rows option">
+        <Link to={`/admins/${this.props.match.params.id}/stores/${storeId}`}><h2>View {storeName}'s Rewards</h2></Link>
       </div>
+      <div className="reward-section rows option">
+        <h2>Reward Customer</h2>
+        <div className="reward-button">
+          <button onClick={this.toggleSearch} className="button round" type="button">Find Customer By Email</button>
+        </div>
+        {search}
+        {users}
+        <div className="reward-button">
+          <button onClick={this.toggleScanner} className="button round" type="button">Scan Customer Code</button>
+        </div>
+        {scanner}
+        <p>{this.state.qrResult}</p>
+      </div>
+    </div>
     )
   }
 }
